624943717db7b540f863c128e96ec46666aa04b6
hugocruzlfc <hugocruzlfc@gmail.com>
2023-11-14 11:23:03 +0100

    feat: adding validators and pipes configurations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -7,5 +8,5 @@ export class AuthService {
 
   signin() {}
 
-  signup() {}
+  signup(authBody: AuthDto) {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 👈 Elimina los campos que no esten en el DTO
+      forbidNonWhitelisted: true, // 👈 Devuelve un error cuando no esten en el DTO
+      transform: true, // 👈 Convierte los tipos de datos a los especificados en el DTO
+    }),
+  ); // 👈 Agregamos el uso de los pipes globales
   await app.listen(4000);
 }
 bootstrap();
